d7875a01d04a68a611fb405e4056542ca4c00128
Roma Stolyarov <romka@Romas-MBP.home>
2025-03-28 11:27:32 -0700

    feat: Modified rendering home page using API

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, make_response
+from flask_restful import Resource
+from app import app, api
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Our dictionary of tasks
+tasks = []
+
+# Handling requests to root URL
+class Index(Resource):
+    def get(self):
+        return make_response(render_template('index.html', tasks=tasks), 200)
+
+api.add_resource(Index, '/')
